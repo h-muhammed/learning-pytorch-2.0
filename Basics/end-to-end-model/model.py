dec37297992a9eff14e673c34af1076a0b719f77
unknown <mshossaincse@gmail.com>
2023-04-30 15:43:07 +0600

    functional end to end deep leaning model

---
+++ Basics/end-to-end-model/model.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn 
+
+#the model
+class BasicNet(nn.Module):
+    """
+    
+    """
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        """
+        
+        """
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+    
